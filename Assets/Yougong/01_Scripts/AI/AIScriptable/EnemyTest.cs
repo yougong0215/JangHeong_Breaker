ba613be319aa98978b402082f2ca384d0fb44db6
KoDaMin <ko060614@gmail.com>
2023-04-13 17:58:36 +0900

    ..

--- Assets/Yougong/01_Scripts/AI/AIScriptable/EnemyTest.cs
+++ Assets/Yougong/01_Scripts/AI/AIScriptable/EnemyTest.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class EnemyTest : EnemyBase
 {
-    public override void Attack()
+
+
+    public override void Attack(Transform target)
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
     }
 
     public override void Init()
     {
-        
+
     }
 }
