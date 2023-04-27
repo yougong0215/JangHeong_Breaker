03d12f788fb269aa3713753fbab65de41be6aaa2
KoDaMin <ko060614@gmail.com>
2023-04-27 18:44:33 +0900

    push in the hole

--- Assets/Guryrea/01_Scripts/RealEnemy/Enemy.cs
+++ Assets/Guryrea/01_Scripts/RealEnemy/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public abstract class Enemy : MonoBehaviour, IDamage
+public abstract class Enemy : PoolAble, IDamage
 {
     public enum EnemyState
     {
